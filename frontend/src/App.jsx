d2805c1b269d59c5e060f2fd4c4a073ea9f4309a
Al <Al@git.com>
2024-02-08 13:48:14 -0500

    small changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
-import LandingPage from './pages/LandingPage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-        <Header />
+        <BrowserRouter>
         <Routes>
             <Route
                 path='/'
                 element={<HomePage />}
             />
-            <Route
-                path='landing'
-                element={<LandingPage />}
-            />
         </Routes>
-        <Footer />
+        </BrowserRouter>
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import { Container } from "reactstrap";
-import LoginForm from "../components/LoginForm";
-import RegistrationForm from "../components/RegistrationForm";
-import SubHeader from "../components/SubHeader";
+import LoginForm from "../components/LoginForm.jsx";
+import RegistrationForm from "../components/RegistrationForm.jsx";
+//import SubHeader from "../components/SubHeader.jsx";
 
 
 const HomePage = () => {
     return (
         <Container>
-            <SubHeader current='Home' />
+            
             <LoginForm />
-            <RegistrationForm />
+            
         </Container>
     )
 };

55c0a92edd16bc824052c8ae4014fd7a09875b57
Al <Al@git.com>
2024-02-09 17:21:24 -0500

    maps

--- frontend/src/components/MessageCard.jsx
+++ frontend/src/components/MessageCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
-const MessageCard = ({username, handleUsername}) => {
+const MessageCard = ({ username, handleUsername }) => {
   const [message, setMessage] = useState(null);
   console.log(username)
 
@@ -11,14 +11,14 @@ const MessageCard = ({username, handleUsername}) => {
         // Fetch message from the backend API based on username
         console.log("username")
         console.log(username)
-        
+
         const response = await fetch(`/api/message/${username}`);
 
         if (!response.ok) {
           throw new Error('Failed to fetch message');
         }
         const data = await response.json();
-        
+
         console.log("in use effect")
         console.log(data)
         setMessage(data);
@@ -28,18 +28,38 @@ const MessageCard = ({username, handleUsername}) => {
     };
 
     fetchMessage();
+    console.log("mapping!!!")
+    message.map((elem) => {
+      console.log(elem)
+    })
   }, []);
 
   return (
     <div>
-      {message && (
-        <Card>
-          <CardBody>
-            <CardTitle>Message from {username}</CardTitle>
-            <CardText>{message}</CardText>
-          </CardBody>
-        </Card>
-      )}
+
+      {
+        
+
+
+
+        /*
+        message.map((elem) => {
+          <Card key={index}>
+            <CardBody>
+              {
+                elem.map((val) => {
+                  val.messages.map((msg) => {
+                    <CardText>{msg}</CardText>
+                  })
+
+                }
+                )}
+            </CardBody>
+          </Card>
+        })//*/
+      }
+
+
     </div>
   );
 };

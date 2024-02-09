f010cb015e0dec8848506c2811ef14bf6f2d0fda
Al <Al@git.com>
2024-02-09 11:35:11 -0500

    python script integrated into post

--- Server/main_server.js
+++ Server/main_server.js
@@ -69,6 +69,7 @@ app.get("/api/authentication/login/:username/:password", async (req, res) => {
 app.get("/api/message/:sender_id", async (req, res) => {
   // res.send(people);
   const id = req.params.sender_id;
+  console.log(id)
 
   try {
     const client = await MongoClient.connect(url);
@@ -90,15 +91,64 @@ app.get("/api/message/:sender_id", async (req, res) => {
   }
 });
 
-//expects 3 params (sender_id, reciever_id, and anonymous) and 1 body
-//
 app.post("/api/message/:sender_id/:reciever_id/:anonymous", async (req, res) => {
   const pars = req.params;
   const message = req.body.message;
-
-  let pyshell = new PythonShell('my_script.py');
+  let pyshell = new PythonShell('./Server/Notebook/ps.py');
   pyshell.send(message);
 
+  pyshell.on('message', async function (message_return) {
+    //received a message sent from the Python script (a simple "print" statement)
+    console.log(message_return)
+    let prediction;
+    if (message_return == 1) {
+      prediction = "positive"
+    } else {
+      prediction = "negative"
+    }
+    console.log("prediction")
+    console.log(prediction)
+
+    const client = await MongoClient.connect(url);
+    const db = client.db('user_messages');
+    const user_collection = db.collection('users');
+    const messages_collection = db.collection('messages');
+
+    const n_id = [[pars.sender_id, pars.reciever_id].sort().join("-"), [pars.anonymous]]
+
+    if (!(await messages_collection.findOne({ "msg_id": n_id })) && (await user_collection.findOne({ 'user_id': pars.reciever_id })) && (await user_collection.findOne({ 'user_id': pars.sender_id }))) {
+      const n_id = [[pars.sender_id, pars.reciever_id].sort().join("-"), [pars.anonymous]]
+      await user_collection.updateOne(
+        { 'user_id': pars.reciever_id },
+        { $push: { "message_lst": n_id } }
+      );
+      await user_collection.updateOne(
+        { 'user_id': pars.sender_id },
+        { $push: { "message_lst": n_id } }
+      );
+      await messages_collection.insertOne(
+        {
+          "msg_id": n_id,
+          "messages": [pars.sender_id, prediction, req.body.message]
+        }
+      );
+      res.send(200)
+
+    } else {
+      res.send(400)
+    }
+  });
+
+});
+//expects 3 params (sender_id, reciever_id, and anonymous) and 1 body
+//
+
+/*
+app.post("/api/message/:sender_id/:reciever_id/:anonymous", async (req, res) => {
+ 
+
+  let pyshell = new PythonShell('./Server/Notebook/ps.py');
+  pyshell.send(message);
 
   const client = await MongoClient.connect(url);
   const db = client.db('user_messages');
@@ -106,47 +156,51 @@ app.post("/api/message/:sender_id/:reciever_id/:anonymous", async (req, res) => 
   const messages_collection = db.collection('messages');
 
   const n_id = [[pars.sender_id, pars.reciever_id].sort().join("-"), [pars.anonymous]]
-  console.log(n_id)
 
   const message_exsts = await messages_collection.findOne({ "msg_id": n_id });
   const reciever = await user_collection.findOne({ 'user_id': pars.reciever_id });
 
   const sender = await user_collection.findOne({ 'user_id': pars.sender_id });
 
+  let prediction;
+
+  pyshell.on('message', async function (message_return) {
+    //received a message sent from the Python script (a simple "print" statement)
+    console.log(message_return)
+    if (message_return == 1) {
+      prediction = "positive"
+    } else {
+      prediction = "negative"
+    }
+    console.log("prediction")
+    console.log(prediction)
+  });
+
   if (!message_exsts && reciever && sender) {
-    //this is a new conversation 
-
-    //console.log(reciever)
-    //console.log(sender)
     const n_id = [[pars.sender_id, pars.reciever_id].sort().join("-"), [pars.anonymous]]
-    console.log(n_id)
-
     const reciever1 = await user_collection.updateOne(
       { 'user_id': pars.reciever_id },
       { $push: { "message_lst": n_id } }
     );
-
     const sender1 = await user_collection.updateOne(
       { 'user_id': pars.sender_id },
       { $push: { "message_lst": n_id } }
     );
-
     const message1 = await messages_collection.insertOne(
       {
         "msg_id": n_id,
-        "messages": [pars.sender_id, req.body.message]
+        "messages": [pars.sender_id, prediction, req.body.message]
       }
     );
     res.send(200)
 
   } else {
-    //this is an existing conversation 
     res.send(400)
   }
-
-  console.log(message)
-
 });
+
+//*/
+//app.put()
 
 //const people = await collection.find().toArray();
 
